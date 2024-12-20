3da0f59d7e1472ab1cc229b833052b05e1779a72
Abel h <abelhailu320@gmail.com>
2024-12-20 12:49:48 -0600

    structured controller and service

---
+++ src/main/java/com/ab/quizapp/service/QuestionService.java
@@ -0,0 +1,10 @@
+package com.ab.quizapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+    public static String getAllQuestions() {
+    
+    }
+}
